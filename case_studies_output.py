d9ce6cd0d8bc33b68c713598432d1d5d44784fc9
Brendan Patch <brendanpatch@gmail.com>
2021-09-27 15:31:08 +1000

    Update case_studies_output.py
    Updated to reflect latest version of birdepy

--- case_studies_output.py
+++ case_studies_output.py
@@ -3,6 +3,7 @@ This file contains the output as displayed by running
 'case_studies_code.py'.
 """
 
+
 #  Table: BLACK ROBIN DATASET PARAMETER ESTIMATES
 # \begin{tabular}{lrrr}
 # \hline
@@ -10,7 +11,7 @@ This file contains the output as displayed by running
 # \hline
 #  Verhulst 1    &  0.3516 & 0.2391 &       0.0023 \\
 #  Verhulst 2    &  0.3018 & 0.1860 &       0.0046 \\
-#  Ricker        &  0.3587 & 0.2380 &       0.0029 \\
+#  Ricker        &  0.3588 & 0.2380 &       0.0029 \\
 #  Beverton-Holt &  0.3690 & 0.2367 &       0.0038 \\
 #  Hassell       &  0.3634 & 0.2373 &       0.0017 \\
 #  MS-S          &  0.3283 & 0.2413 &       0.0045 \\
@@ -34,15 +35,15 @@ This file contains the output as displayed by running
 # Table: BLACK ROBIN DATASET CARRYING CAPACITY AND LIKELIHOOD
 # \begin{tabular}{llr}
 # \hline
-#  Model         & Capacity           &   Likelihood \\
+#  Model         & Capacity   &   Likelihood \\
 # \hline
-#  Verhulst 1    & 137.47225027869425 &  9.8414e-22  \\
-#  Verhulst 2    & 134.684228735941   &  9.5712e-22  \\
-#  Ricker        & 141.18712309970493 &  9.99087e-22 \\
-#  Beverton-Holt & 145.5318682607531  &  1.01644e-21 \\
-#  Hassell       & 143.27082311485208 &  1.00745e-21 \\
-#  MS-S          & 132.1534401373272  &  9.27018e-22 \\
-#  linear        & n/a                &  5.58725e-22 \\
+#  Verhulst 1    & 137.0      &  9.8414e-22  \\
+#  Verhulst 2    & 135.0      &  9.5712e-22  \\
+#  Ricker        & 141.0      &  9.99087e-22 \\
+#  Beverton-Holt & 146.0      &  1.01644e-21 \\
+#  Hassell       & 143.0      &  1.00745e-21 \\
+#  MS-S          & 132.0      &  9.27018e-22 \\
+#  linear        & n/a        &  5.58725e-22 \\
 # \hline
 # \end{tabular}
 
@@ -54,11 +55,11 @@ This file contains the output as displayed by running
 #  Verhulst 1       &  0.1998 & 0.1492 &       0.0008 \\
 #  Verhulst 2       &  0.1931 & 0.1423 &       0.0011 \\
 #  Ricker           &  0.1999 & 0.1493 &       0.0008 \\
-#  Beverton-Holt    &  0.2003 & 0.1496 &       0.0008 \\
+#  Beverton-Holt    &  0.1997 & 0.1492 &       0.0008 \\
 #  Hassell          &  0.1999 & 0.1493 &       0.0004 \\
-#  MS-S             &  0.1966 & 0.1494 &       0.0025 \\
+#  MS-S             &  0.1966 & 0.1493 &       0.0025 \\
 #  linear           &  0.1902 & 0.1506 &              \\
-#  linear-migration &  0.1813 & 0.1489 &       0.3125 \\
+#  linear-migration &  0.1812 & 0.1489 &       0.3157 \\
 # \hline
 # \end{tabular}
 # Table: WHOOPING CRANE STANDARD ERRORS
@@ -69,25 +70,25 @@ This file contains the output as displayed by running
 #  Verhulst 1       &  0.0351 & 0.0293 &  0.0013 \\
 #  Verhulst 2       &  0.0303 & 0.0321 &  0.0021 \\
 #  Ricker           &  0.0354 & 0.0293 &  0.0015 \\
-#  Beverton-Holt    &  0.0359 & 0.0294 &  0.0016 \\
+#  Beverton-Holt    &  0.0357 & 0.0293 &  0.0016 \\
 #  Hassell          &  0.0356 & 0.0293 &  0.0008 \\
 #  MS-S             &  0.0320 & 0.0293 &  0.0022 \\
 #  linear           &  0.0295 & 0.0293 &         \\
-#  linear-migration &  0.0317 & 0.0294 &  0.4765 \\
+#  linear-migration &  0.0317 & 0.0294 &  0.4770 \\
 # \hline
 # \end{tabular}
 # Table: WHOOPING CRANE CARRYING CAPACITY AND LIKELIHOOD
 # \begin{tabular}{llr}
 # \hline
-#  Model            & Capacity           &   Likelihood \\
+#  Model            & Capacity   &   Likelihood \\
 # \hline
-#  Verhulst 1       & 329.92867802274594 &  1.50976e-81 \\
-#  Verhulst 2       & 324.92957217434537 &  1.52411e-81 \\
-#  Ricker           & 366.80121622296633 &  1.5053e-81  \\
-#  Beverton-Holt    & 406.13567548426556 &  1.5011e-81  \\
-#  Hassell          & 387.61905364145827 &  1.50321e-81 \\
-#  MS-S             & 223.121195684182   &  1.56307e-81 \\
-#  linear           & n/a                &  1.29697e-81 \\
-#  linear-migration & n/a                &  1.6284e-81  \\
+#  Verhulst 1       & 330.0      &  1.50976e-81 \\
+#  Verhulst 2       & 325.0      &  1.52411e-81 \\
+#  Ricker           & 367.0      &  1.5053e-81  \\
+#  Beverton-Holt    & 412.0      &  1.50119e-81 \\
+#  Hassell          & 388.0      &  1.50321e-81 \\
+#  MS-S             & 223.0      &  1.56307e-81 \\
+#  linear           & n/a        &  1.29697e-81 \\
+#  linear-migration & n/a        &  1.62844e-81 \\
 # \hline
 # \end{tabular}
